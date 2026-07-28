1947d7849d07cb4466fb87ce9208fb82c7f9f781
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-28 18:19:00 +0530

    Create #try_finally.py

---
+++ ADVANCED PYTHON1/#try_finally.py
@@ -0,0 +1,11 @@
+def main():
+    try:
+        a = int(input("enter o"))
+        print(a)
+        return
+    except Exception as e:
+        print(e)
+        return
+    finally:
+        print("ghfj")
+main()
