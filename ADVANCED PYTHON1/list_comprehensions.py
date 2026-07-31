ea53ffe85625282e1bc54660d1599f5321f07bea
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-31 15:35:01 +0530

    Update list_comprehensions.py

--- ADVANCED PYTHON1/list_comprehensions.py
+++ ADVANCED PYTHON1/list_comprehensions.py
@@ -1,1 +1,15 @@
 
+# Demonstrating squares of list items in Python
+
+l = [4, 8, 7, 9, 9, 0]
+
+# Method 1: Using for loop
+squaredlist = []
+for item in l:
+    squaredlist.append(item * item)
+
+print(squaredlist)
+
+# Method 2: Using list comprehension (cleaner)
+squaredlist = [i * i for i in l]
+print(squaredlist)
