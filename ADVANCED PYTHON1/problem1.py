f39a51e6a3d65033a40f35bed7ae268520c359b3
Bhoomi Jain <bhoomijn4@gmail.com>
2026-07-31 18:53:29 +0530

    Update problem1.py

--- ADVANCED PYTHON1/problem1.py
+++ ADVANCED PYTHON1/problem1.py
@@ -1,1 +1,22 @@
 
+# Demonstrating file handling with exception handling in Python
+
+try:
+    with open("1.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+    with open("2.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+try:
+    with open("3.txt", "r") as f:
+        print(f.read())
+except Exception as e:
+    print(e)
+
+print("Thank you")
