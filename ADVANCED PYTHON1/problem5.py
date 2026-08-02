4f2d4cf85a8503f286384d6eb2da7b4d9c15419c
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-02 16:15:31 +0530

    Implement multiplication table file writing
    Added functionality to write a multiplication table to a file.

--- ADVANCED PYTHON1/problem5.py
+++ ADVANCED PYTHON1/problem5.py
@@ -1,1 +1,8 @@
 
+# Demonstrating writing multiplication table to a file
+
+n = int(input("Enter a number: "))
+table = [n * i for i in range(1, 11)]
+
+with open("1.txt", "a") as f:   # file extension ko sahi rakho (.txt)
+    f.write(str(table) + "\n")
