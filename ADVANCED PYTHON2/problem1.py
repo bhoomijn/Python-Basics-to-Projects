f561c79b718e191084325931e90eb7c9f21e04e1
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-04 17:50:08 +0530

    Update problem1.py

--- ADVANCED PYTHON2/problem1.py
+++ ADVANCED PYTHON2/problem1.py
@@ -1,1 +1,9 @@
 
+# Program to input student details and format using format function
+
+name = input("Enter name: ")
+marks = int(input("Enter marks: "))
+phone = int(input("Enter phone number: "))
+
+s = "{} got {} marks and phone number is {}".format(name, marks, phone)
+print(s)
