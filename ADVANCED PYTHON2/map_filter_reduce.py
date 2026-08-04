def4b913d65c826c52c40daaae030505cc14b6b4
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-04 16:39:39 +0530

    Update map_filter_reduce.py

--- ADVANCED PYTHON2/map_filter_reduce.py
+++ ADVANCED PYTHON2/map_filter_reduce.py
@@ -1,1 +1,20 @@
 
+# Map Example
+
+l = [1,8,5,3,6,2,4,7]
+
+square = lambda n : n*n
+
+xList = map(square, l)
+print(list(xList))
+
+# Filter Example
+
+def even(n):
+    if n%2 == 0:
+        return True
+    else:
+        return False
+
+yList = filter(even, l)
+print(list(yList))
