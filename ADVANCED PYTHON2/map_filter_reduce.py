280b7dcccea9d717b1b7610cfa42d4e2a35e185b
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-04 16:51:30 +0530

    Update map_filter_reduce.py

--- ADVANCED PYTHON2/map_filter_reduce.py
+++ ADVANCED PYTHON2/map_filter_reduce.py
@@ -18,3 +18,29 @@ def even(n):
 
 yList = filter(even, l)
 print(list(yList))
+
+
+
+# Reduce Example: This is a simple example of how to use the venv module to create a virtual environment in Python.
+
+
+
+from functools import reduce
+
+
+l = [1, 8, 5, 3, 6, 2, 4, 7]
+
+# Define a function to add two numbers
+def add(x, y):
+    return x + y
+
+# Use reduce to sum all elements in the list
+result = reduce(add, l)
+print(result)
+
+
+# Reduce another Example: 
+def sum(a, b):
+    return a + b
+print(reduce(sum, l))  # Output: 36
+
