92cf665370241e6283453977e944ff07c6820b6b
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-05 11:20:43 +0530

    Update problem3.py

--- ADVANCED PYTHON2/problem3.py
+++ ADVANCED PYTHON2/problem3.py
@@ -1,1 +1,19 @@
 
+# Program: Check divisibility by 5
+# Author: Bhoomi
+# Language: Python 3
+
+def check_divisible_by_5(numbers: list) -> None:
+    """
+    Print numbers from the list that are divisible by 5.
+    
+    Args:
+        numbers (list): List of integers.
+    """
+    for num in numbers:
+        if num % 5 == 0:
+            print(f"The {num} is divisible by 5")
+
+if __name__ == "__main__":
+    numbers = [67, 5, 25, 30, 9, 33]
+    check_divisible_by_5(numbers)
