f5f5cc1a04dd367702a761d3ced740c7069cae36
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-05 11:36:03 +0530

    Update problem4.py

--- ADVANCED PYTHON2/problem4.py
+++ ADVANCED PYTHON2/problem4.py
@@ -1,1 +1,21 @@
 
+# Program: Find Maximum Value in a List
+# Author: Bhoomi
+# Language: Python 3
+
+def find_maximum(numbers: list) -> int:
+    """
+    Return the maximum value from a list of integers.
+    
+    Args:
+        numbers (list): List of integers.
+    
+    Returns:
+        int: Maximum value in the list.
+    """
+    return max(numbers)
+
+if __name__ == "__main__":
+    numbers = [88, 89, 545, 77]
+    maximum = find_maximum(numbers)
+    print(f"The maximum value in the list is: {maximum}")
