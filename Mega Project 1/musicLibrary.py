1a2713889920cbdd667f4f873e7ebecddcf7e7e0
Bhoomi Jain <bhoomijn4@gmail.com>
2026-08-07 22:24:47 +0530

    Update musicLibrary.py

--- Mega Project 1/musicLibrary.py
+++ Mega Project 1/musicLibrary.py
@@ -1,1 +1,9 @@
 
+music = {
+    "stealth": "https://www.youtube.com/watch?v=3tmd-ClpJxA",
+    "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "choo lo": "https://youtu.be/JcvPhoXreRM",
+    "nira": "https://www.youtube.com/watch?v=UrsmFxEIp5k",
+    "kesariya": "https://www.youtube.com/watch?v=BddP6PYo2gs",
+    "believer": "https://www.youtube.com/watch?v=7wtfhZwyrcc"
+}
